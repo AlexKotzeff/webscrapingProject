b58a757cfd7888e804ef74cab5d1e4795b138b6f
Alex Kotzeff <agkotzeff@gmail.com>
2023-08-16 14:50:41 -0400

    Finding a book in the CSV
    Updates to find a specific book in the csv file

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # This is a practice program that uses a webscraping sandbox called "books.toscrape.com". This program is to both
 # refresh my coding skills in python and to learn about webscraping. While I could check the robots.txt for an actual
 # website, I do not want to risk taking down their site. This also avoids any ethical or legal issues that I do not
-# have to knowledge to navigate yet.
+# have the knowledge to navigate yet.
 
 # This code uses the requests library to access the web and then the BeautifulSoup package to read the information.
 
@@ -10,10 +10,13 @@
 
 import csv
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 
 
-def run_scrape():
+# This function moves through the catalogue of books to gather all the book titles and the link to access more
+# information about them
+def run_catalogue_scrape():
     start_url = "http://books.toscrape.com/catalogue/page-1.html"
 
     # Create a csv file to save all the books to.
@@ -29,42 +32,71 @@ def run_scrape():
     while next_page:
         req = requests.get(this_page)
         soup = BeautifulSoup(req.content, "html.parser")
-        booklinks = soup.findAll("li", {"class": "col-xs-6 col-sm-4 col-md-3 col-lg-3"})
+        allbooks = soup.findAll("li", {"class": "col-xs-6 col-sm-4 col-md-3 col-lg-3"})
 
-        for links in booklinks:
+        for book in allbooks:
             # Using the html of the site, we can get the href so that each book can be inspected individually if needed.
-            book_title = links.h3.a["title"]
-            book_link = links.h3.a["href"]
+            book_title = book.h3.a["title"]
+            book_link = book.h3.a["href"]
             f.write("\"" + book_title + "\"" + ", http://books.toscrape.com/catalogue/" + book_link + "\n")
 
         # Check if there is a next button in the page meaning that we need to continue scraping.
         next_button = soup.find("li", {"class": "next"})
         if next_button is not None:
             this_page = "http://books.toscrape.com/catalogue/" + next_button.a["href"]
-            print(this_page)
             next_page = True
         else:
             next_page = False
-
     f.close()
 
 
-def find_book(book_to_find):
-    with open("Books.csv") as obj:
-        reader = csv.reader(obj, delimiter=',')
+def run_book_scrape(book):
+    url = book.title
+    print(url)
 
-        for line in reader:
-            if book_to_find in str(line):
-                print(line[1])
+# This function moves through the csv of books to try and find what the user is looking for. It uses the pandas library
+# for speed and simplicity.
+def find_in_csv(book_to_find):
+    df = pd.read_csv("Books.csv")
+    found = df[df["Book title"].str.contains(book_to_find, case=False)]
+
+    return found
+
+
+def user_book_input():
+    book_name = input("\nPlease enter the name of the book you would like to search for: ")
+
+    found = find_in_csv(book_name)
+    count = found.shape[0]
+
+    if count == 0:
+        print("\nNo books with that name were found in the catalogue. Please try again")
+        user_book_input()
+    elif count > 1:
+        print("\nSeveral books matching that name were found.")
+        for title in found["Book title"]:
+            print(title)
+        user_book_input()
+    else:
+        print("\nThe book was found in the catalogue!\n")
+
+    return found
+
 
 
 if __name__ == '__main__':
+    # Check if the user wants to refresh the scrape since last time.
     run_again = input("Would you like to run the scrape again? (Yes/No): ")
-
     if run_again == "Yes":
-        run_scrape()
+        run_catalogue_scrape()
 
-    book_name = input("Please enter the name of the book you would like to search for: ")
-    find_book(book_name)
+    # Get the book from the user and check if it is in the catalogue
+    book_info = user_book_input()
+
+    run_book_scrape(book_info)
+
+
 
     keyword = input("Please enter the word you would like to search for in the description of the book: ")
+
+
